ca18df7b597f5996073498f2c74cfd52bbd72c52
TonLeon <55150238+TonLeon@users.noreply.github.com>
2021-09-29 15:42:53 -0500

    init_commit

---
+++ app.py
@@ -0,0 +1,151 @@
+from flask import Flask, render_template, request, jsonify
+import pymongo
+import json
+
+app = Flask(__name__)
+def get_poems_titles(name_of_db):
+    client = pymongo.MongoClient('mongodb://localhost:27017')
+    db = client['admin']
+    collection= db[name_of_db]
+    titles = [title for title in collection.find({"root": []})]
+    duplicates = [title for title in collection.find({"root": {'$ne':[]}})]
+    non_dup_id_2_next_nondup = {titles[i]['ID']: titles[i + 1]['ID'] for i in range(len(titles) - 1)}
+    non_dup_id_2_previous_nondup = {titles[i]['ID']: titles[i - 1]['ID'] for i in range(1, len(titles))}
+    return titles, duplicates, non_dup_id_2_next_nondup, non_dup_id_2_previous_nondup
+
+
+def get_poems_texts(ID, name_of_db):
+    client = pymongo.MongoClient('mongodb://localhost:27017')
+    db = client['admin']
+    collection = db[name_of_db]
+    count = collection.count_documents({})
+    poem_texts = [text for text in collection.find({"poem_text": {"$exists": True}, "ID":ID})] #вот тут словарь в списке
+    duplicates = [title for title in collection.find({"root": {'$ne':[]}})]
+    return poem_texts, duplicates, count 
+
+def search_result(word):
+    client = pymongo.MongoClient('mongodb://localhost:27017')
+    db = client['admin']
+    all_collections = db.list_collection_names()
+    
+    all_texts = []
+    
+    for collection in all_collections:
+        if collection.startswith('Shvarts'):
+            collection = db['{}'.format(collection)]
+            texts_in_collection = [text for text in collection.find({"$text": {"$search": word}}, 
+                     projection={'_id':False}).sort('meta.date_written')]
+            all_texts = all_texts + texts_in_collection
+
+    poems = []
+
+    for text in all_texts:
+        lst = []
+        lst.append(text.get('ID'))
+        lst.append(text.get('title'))
+        lst.append(text.get('meta').get('edition'))
+        lst.append(text.get('meta').get('date_written'))
+        lst.append(text.get('path'))
+        poems.append(lst)
+
+    return sorted(poems, key=lambda x: x[1])
+
+def filter_poems_by_year(name_of_db, start_year, end_year):
+    client = pymongo.MongoClient('mongodb://localhost:27017')
+    db = client['admin']
+    collection = db[name_of_db]
+    texts_of_exact_period = []
+    for text in collection.find({'meta.date_written':{'$gte':start_year, '$lte':end_year}, "root": []}).sort('meta.date_written', pymongo.ASCENDING):
+        texts_of_exact_period.append(text)
+
+    poems = []
+
+    for text in texts_of_exact_period:
+        lst = []
+        lst.append(text.get('ID'))
+        lst.append(text.get('title'))
+        lst.append(text.get('meta').get('date_written'))
+        poems.append(lst)
+
+    return poems
+
+# app.jinja_env.filters['poems_of_period'] = filter_poems_by_year
+
+@app.route('/')
+def base():
+    return render_template('index.html', page_name="shvarts")
+
+@app.route('/about/')
+def index():
+    return render_template('index.html', page_name='project')
+
+@app.route('/list_of_texts/')
+def get_list_of_texts():
+	titles, duplicates, _, _ = get_poems_titles('Shvarts_60')
+	return render_template('titles.html', page_name='list_of_texts', 
+							titles=titles, duplicates=duplicates)
+
+@app.route('/filter_poems_by_period')
+def filter_poems():
+    try:
+        result = filter_poems_by_year('Shvarts_60', 1960, 1969)
+        return jsonify(result=result)
+    except Exception as e:
+        return str(e)
+
+@app.route('/texts_60_<int:ID>/')
+def get_text(ID):
+    _, _, non_dup_id_2_next_nondup, non_dup_id_2_previous_nondup = get_poems_titles('Shvarts_60')
+    previous_id = non_dup_id_2_previous_nondup[ID] if ID in non_dup_id_2_previous_nondup.keys() else None
+    next_id = non_dup_id_2_next_nondup[ID] if ID in non_dup_id_2_next_nondup.keys() else None
+    poem_texts, duplicates, _ = get_poems_texts(ID, 'Shvarts_60')
+    return render_template('text.html', page_name='texts', 
+							poem_texts=poem_texts, duplicates=duplicates, next_id=next_id, previous_id=previous_id)
+
+@app.route('/printed_edition/')
+def get_list_of_texts_from_printed_editions():
+	titles, duplicates, _, _ = get_poems_titles('Shvarts_ZT')
+	return render_template('printed_edition.html', page_name='texts_from_printed_editions', 
+							titles=titles, duplicates=duplicates)
+
+@app.route('/texts_ZT_<int:ID>/')
+def get_ZT_text(ID):
+    poem_texts, duplicates, count = get_poems_texts(ID, 'Shvarts_ZT')
+    titles, _, _, _ = get_poems_titles('Shvarts_ZT')
+    return render_template('text_from_printed_edition.html', page_name='texts', 
+							poem_texts=poem_texts, duplicates=duplicates, count=count, titles=titles)
+
+@app.route('/background_process', methods=['GET', 'POST', 'DELETE', 'PATCH'])
+def background_process():
+    try:
+        word = request.args.get('search', 'text', type=str)
+        result = search_result(word)
+        print(result)
+        return jsonify(result=result)
+    except Exception as e:
+        return str(e)
+
+@app.route('/contrib/')
+def photos():
+    return render_template('contrib.html', page_name='photos')
+
+@app.route('/other/')
+def other():
+    return render_template('other.html', page_name='other')
+
+@app.route('/bibl/')
+def bibl():
+    return render_template('bibl.html', page_name="bibl")
+
+@app.route('/trial/')
+def trial():
+    return render_template('trial.html', page_name="trial")
+
+@app.route('/comparison_60_<int:ID>/')
+def compare_poems(ID):
+    poem_texts, duplicates, _ = get_poems_texts(ID, 'Shvarts_60')
+    return render_template('comparison.html', poem_texts=poem_texts, duplicates=duplicates)
+
+
+if __name__=='__main__':
+	app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,139 @@
+// Поиск на странице titles.html
+
+
+$(function() {
+  $('button#process_input').bind('click', function() {
+    $.getJSON('/background_process', {
+      search: $('input[name="search"]').val(),
+      }, function(data) {
+              $("div#result").empty();
+              var element = document.getElementById("time_period");
+              element.innerHTML = "Результаты поиска:"; 
+              if (data.result.length !== 0){
+              $(data.result).each(function (index, value) { console.log(value)
+                $("div#result").append('<pre class="titles__poem"><a class="titles__poem" href="../' + value[4] +
+                      value[0] + '/">' + 
+                    value[1]+ '</a>'+ '<p class="bibliography">Год написания: '+ value[3] + 
+                    '</p><p class="bibliography">Опубликовано в: '+ value[2] + '</p></pre>');
+          });
+        }
+              else{
+                $("div#result").append('<pre class="titles__poem">По Вашему запросу ничего не обнаружено. Попробуйте еще раз.</pre>')
+              };
+        });
+        return false;
+        });
+      });
+
+// Выдача стихотворений 60-х по годам на titles.html
+
+$(function() {
+  $('a#sixties').bind('click', function() {
+    $.getJSON('/filter_poems_by_period', function(data) {
+      $("div#result").empty();
+      var element = document.getElementById("time_period");
+      element.innerHTML = "Стихотворения 1960-х годов";
+      var result = data.result
+      let year = result[0][2]
+      $(data.result).each(function (index, value) {
+        if(index > 0){
+          if (value[2] === year){
+        $("div#result").append('<pre class="titles__poem"><a class="titles__poem" href="../texts_60_' + value[0]+ '/">' + value[1] + '</a></pre>');
+      }
+          else{
+            year = value[2]
+            $("div#result").append('<h3 class="year">'+ value[2] +'</h3><pre class="titles__poem"><a class="titles__poem" href="../texts_60_' + value[0]+ '/">' + value[1] + '</a></pre>');
+          };
+  }
+        else{$("div#result").append('<h3 class="year">'+ year +'</h3><pre class="titles__poem"><a class="titles__poem" href="../texts_60_' + value[0]+ '/">' + value[1] + '</a></pre>');
+    }})
+  })
+})})
+    // $.getJSON('/background_process', {console.log(1)})
+
+
+// Сравнение стихотворений в comparison.html
+
+$('#mySelect_1').change(function(){ 
+                var value = $(this).val()
+                $("div#first_poem").empty()
+                $("div#first_poem").append(value); 
+                var meta = $(this).find('option:selected').attr('meta')
+                $("div#first_meta").empty()
+                $("div#first_meta").append(meta)
+
+              });
+                
+                    
+
+$('#mySelect_2').change(function(){ 
+                var value = $(this).val()
+                $("div#second_poem").empty()
+                $("div#second_poem").append(value);
+                var meta = $(this).find('option:selected').attr('meta')
+                $("div#second_meta").empty()
+                $("div#second_meta").append(meta)
+
+              });
+
+// Возврат на предыдущую страницу
+function goBack() {
+  window.history.back();
+}
+
+// Сравнение стихотворений с выделением разночтений
+$(function() {
+  $('button#compare').bind('click', function() {
+highlight($("pre#new"), $("pre#old"));
+function highlight(second_poemElem, first_poemElem){
+  var first = 0;
+  var second = 0;
+  var first_text = '';
+  var second_text = '';
+  var oldText = first_poemElem.text().split('\n');
+  var newText = second_poemElem.text().split('\n');
+  
+  while (first < oldText.length && second < newText.length) {
+
+  if (oldText[first].length === 0) {
+    first_text += oldText[first] + '\n';
+    first = first + 1;
+   } 
+   else if (newText[second].length === 0) {
+    second_text += newText[second] + '\n';
+    second = second + 1;
+   }
+   else if (oldText[first] !== newText[second]) {
+    first_text += "<span class='highlight_old'>"+ oldText[first] +"</span>\n";
+          second_text += "<span class='highlight_new'>"+ newText[second] +"</span>\n";
+    first = first + 1;
+    second = second + 1;
+   } 
+   else {
+    first_text += oldText[first] + '\n';
+    second_text += newText[second] + '\n';
+    first = first + 1;
+    second = second + 1;
+   }}
+   
+  
+  for (var i = first; i < oldText.length; i++) {
+   if (oldText[i] !== newText[second]) {
+    first_text += "<span class='highlight_old'>"+ oldText[i] +"</span>\n";
+   } else {
+    first_text += oldText[i] + '\n';
+   }
+  }
+  
+  for (var i = second; i < newText.length; i++) {
+   if (oldText[first] !== newText[i]) {
+    second_text += "<span class='highlight_new'>"+ newText[i] +"</span>\n";
+   } else {
+    second_text  += newText[i] + '\n';
+   }
+  }
+  
+  first_poemElem.html(first_text);
+  second_poemElem.html(second_text);
+}; 
+})});
